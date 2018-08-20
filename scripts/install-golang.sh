70c9dd7d982dcefe22fe8a6fbadeacbf6870e8ac
Sher Chowdhury <sher.chowdhury@gmail.com>
2018-08-20 12:43:17 +0100

    wip

--- scripts/install-golang.sh
+++ scripts/install-golang.sh
@@ -13,4 +13,13 @@ tar -C /usr/local -xzf ${golang_archive_filename}
 cd /usr/local/go/bin 
 cp -rfp /usr/local/go/bin/* /usr/bin/
 
+
+
+echo "export GOPATH=/root/go_project" > /root/.go_config
+echo "export GOBIN="${GOPATH}/bin"" >> /root/.go_config
+
+echo "source /root/.go_config" >> /root/.bash_profile
+
+
+
 exit 
