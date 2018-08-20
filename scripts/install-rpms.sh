7ff728b0ff8ce552ccf0dd91f9bba40e4f6739b7
Sher Chowdhury <sher.chowdhury@gmail.com>
2018-08-20 11:36:47 +0100

    wip

---
+++ scripts/install-rpms.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -ex
+
+
+echo '##########################################################################'
+echo '##### About to run install-rpms.sh script ##################'
+echo '##########################################################################'
+
+yum install -y epel-release 
+yum install -y vim 
+yum install -y bash-completion 
+yum install -y man-pages
+yum install -y bash-completion-extras
