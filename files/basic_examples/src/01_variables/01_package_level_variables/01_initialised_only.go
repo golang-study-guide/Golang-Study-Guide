ba347d5d2e0985142a50e56cf0da517c71c6ac85
Sher Chowdhury <Sher-Chowdhury@users.noreply.github.com>
2019-07-26 11:22:55 +0100

    Update 01_initialised_only.go

--- files/basic_examples/src/01_variables/01_package_level_variables/01_initialised_only.go
+++ files/basic_examples/src/01_variables/01_package_level_variables/01_initialised_only.go
@@ -7,8 +7,8 @@ import (
 
 // here we declared to variables but we haven't assigned a value to them. 
 // i.e. declared but not yet initialized
-// by default unitialized int variables takes the value '0' and
-// unitialized string variables takes the value ''.
+// by default unitialized int-variables takes the value '0' and
+// unitialized string-variables takes the value ''.
 var (
   name string
   city string
